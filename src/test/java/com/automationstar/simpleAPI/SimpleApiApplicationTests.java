1f8355264e6043792128a857f32c794c3adeefb5
Matthias ZAX <matthias.zax@rbinternational.com>
2022-10-04 11:01:59 +0200

    Add Java Application with simple Api call

---
+++ src/test/java/com/automationstar/simpleAPI/SimpleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.automationstar.simpleAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
